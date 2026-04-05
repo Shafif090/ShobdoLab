a6ada016787ab9034bf07137e16b168cc7733bf6
SHAFIF FERDOUS RAHMAN <shafifferdous@gmail.com>
2026-04-05 19:45:17 +0600

    supabase connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,31 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { createClient } from "@supabase/supabase-js";
 
 dotenv.config();
 const app = express();
 
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY,
+);
+
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('ShobdoLab Backend Running');
+app.get("/", (req, res) => {
+  res.send("ShobdoLab Backend Running");
+});
+
+app.get("/dbtest", async (req, res) => {
+  const { data, error } = await supabase.from("Word").select("*");
+
+  if (error) {
+    return res.status(500).json({ error });
+  }
+
+  res.json(data);
 });
 
 const PORT = process.env.PORT || 5000;
